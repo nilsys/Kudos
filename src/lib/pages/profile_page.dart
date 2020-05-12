55e6e0e5eb692c4f463139a35d9d01f5964e520e
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-12 17:43:36 +0300

    Export ProfileAchievementsList widget

--- src/lib/pages/profile_page.dart
+++ src/lib/pages/profile_page.dart
@@ -1,8 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:kudosapp/models/achievement.dart';
-import 'package:kudosapp/pages/achievement_details_page.dart';
-import 'package:kudosapp/widgets/image_loader.dart';
+import 'package:kudosapp/widgets/profile_achievement_list_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:kudosapp/models/user.dart';
 import 'package:kudosapp/service_locator.dart';
@@ -33,7 +31,7 @@ class ProfilePage extends StatelessWidget {
           return Column(
             children: <Widget>[
               _buildHeader(context, viewModel.user),
-              _buildAchievementsList(viewModel),
+              ProfileAchievementsList(viewModel),
             ],
           );
         },
@@ -66,87 +64,6 @@ class ProfilePage extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-
-  Widget _buildAchievementsList(ProfileViewModel viewModel) {
-    return FutureBuilder<List<Achievement>>(
-      future: viewModel.achievements,
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return _buildLoading();
-          case ConnectionState.done:
-            if (snapshot.hasError) {
-              return _buildError(snapshot.error);
-            } else if (snapshot.hasData && snapshot.data.isNotEmpty) {
-              return _buildList(snapshot.data);
-            } else {
-              return _buildEmpty();
-            }
-            break;
-          case ConnectionState.active:
-          case ConnectionState.none:
-          default:
-            return _buildError("Unknown state");
-        }
-      },
-    );
-  }
-
-  Widget _buildLoading() {
-    return Expanded(
-      child: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
-  }
-
-  Widget _buildError(Object error) {
-    return Center(
-      child: Text(
-        "Error: $error", // TODO YP: temporary
-        style: TextStyle(
-          color: Colors.red,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildEmpty() {
-    return Center(
-      child: Text("No data"), // TODO YP: temporary
-    );
-  }
-
-  Widget _buildList(List<Achievement> achievements) {
-    return Expanded(
-      child: GridView.builder(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20,
-        ),
-        shrinkWrap: true,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: 1,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: achievements.length,
-        itemBuilder: (context, index) =>
-            _buildListItem(context, achievements[index]),
-      ),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, Achievement achievement) {
-    return InkWell(
-      child: ImageLoader(
-        url: achievement.imageUrl,
-      ),
-      onTap: () {
-        Navigator.of(context).push(AchievementDetailsRoute(achievement));
-      },
     );
   }
 }
