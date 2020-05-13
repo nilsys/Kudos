2de00033a047c756d49fe7d5d08baa8035bab2ef
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-14 00:11:19 +0300

    Add collect's model

---
+++ src/lib/models/user_achievement_collection.dart
@@ -0,0 +1,8 @@
+import 'package:kudosapp/models/user_achievement.dart';
+
+class UserAchievementCollection {
+  final UserAchievement achievement;
+  final int count;
+
+  const UserAchievementCollection(this.achievement, this.count);
+}
