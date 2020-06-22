07b012e803b9ee4bac0161e5aa20d51bf79d50e3
savikpavel <pavel.savik@softeq.com>
2020-06-23 01:00:53 +0300

    TopDecorator added to PeoplePage; Elevation param added to GradientAppBar

--- src/lib/widgets/gradient_app_bar.dart
+++ src/lib/widgets/gradient_app_bar.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:kudosapp/kudos_theme.dart';
 
 class GradientAppBar extends AppBar {
-  GradientAppBar({String title, List<Widget> actions})
+  GradientAppBar({String title, List<Widget> actions, double elevation})
       : super(
           title: Text(
             title ?? "",
             style: KudosTheme.appBarTitleTextStyle,
           ),
+          elevation: elevation,
           actions: actions,
           flexibleSpace: _buildGradientBackground(),
         );
 
-  GradientAppBar.withWidget(Widget titleWidget, {List<Widget> actions})
+  GradientAppBar.withWidget(Widget titleWidget, {List<Widget> actions, double elevation})
       : super(
           title: titleWidget,
           actions: actions,
+          elevation: elevation,
           flexibleSpace: _buildGradientBackground(),
         );
 
