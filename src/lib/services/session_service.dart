823b75bf7176dfcfa2723d1a25e8d75771f3ee64
savikpavel <pavel.savik@softeq.com>
2020-08-06 14:06:15 +0300

    Close subscriptions added for TeamsService

--- src/lib/services/session_service.dart
+++ src/lib/services/session_service.dart
@@ -1,11 +1,13 @@
 import 'package:kudosapp/service_locator.dart';
 import 'package:kudosapp/services/data_services/achievements_service.dart';
 import 'package:kudosapp/services/base_auth_service.dart';
+import 'package:kudosapp/services/data_services/teams_service.dart';
 import 'package:kudosapp/services/push_notifications_service.dart';
 import 'package:kudosapp/services/data_services/users_service.dart';
 
 class SessionService {
   final _usersService = locator<UsersService>();
+  final _teamsService = locator<TeamsService>();
   final _authService = locator<BaseAuthService>();
   final _achievementsService = locator<AchievementsService>();
   final _pushNotificationsService = locator<PushNotificationsService>();
@@ -20,6 +22,7 @@ class SessionService {
   Future<void> closeSession() async {
     await _pushNotificationsService.unsubscribeFromNotifications();
     _usersService.closeSubscriptions();
+    _teamsService.closeSubscriptions();
     _achievementsService.closeSubscriptions();
     await _authService.signOut();
   }
