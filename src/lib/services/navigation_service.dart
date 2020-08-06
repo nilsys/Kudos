295487209280832d727dc8d3526b9597c3643fd5
savikpavel <pavel.savik@softeq.com>
2020-08-06 22:38:47 +0300

    Material page route added to navigation service

--- src/lib/services/navigation_service.dart
+++ src/lib/services/navigation_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:kudosapp/viewmodels/base_viewmodel.dart';
 import 'package:provider/provider.dart';
@@ -7,18 +8,26 @@ class NavigationService {
       Navigator.of(context).pop(result);
 
   Future<T> navigateTo<T>(BuildContext context, Widget page) =>
-      Navigator.of(context).push(_getPageRouteBuilder(page));
+      Navigator.of(context).push(_getMaterialPageRoute(page));
 
-  Future navigateToViewModel<T extends BaseViewModel>(
+  Future<TResult>
+      navigateToViewModel<TViewModel extends BaseViewModel, TResult>(
     BuildContext context,
     Widget page,
-    T viewModel,
+    TViewModel viewModel,
   ) {
-    var wrappedPage = ChangeNotifierProvider<T>(
+    var wrappedPage = ChangeNotifierProvider<TViewModel>(
       create: (context) => viewModel,
       child: page,
     );
     return navigateTo(context, wrappedPage);
+  }
+
+  MaterialPageRoute<T> _getMaterialPageRoute<T>(Widget page) {
+    return MaterialPageRoute(
+      builder: (context) => page,
+      fullscreenDialog: true,
+    );
   }
 
   PageRouteBuilder<T> _getPageRouteBuilder<T>(Widget page) {
@@ -26,13 +35,28 @@ class NavigationService {
       fullscreenDialog: true,
       transitionDuration: Duration(milliseconds: 500),
       pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return FadeTransition(
-          opacity:
-              animation, // CurvedAnimation(parent: animation, curve: Curves.elasticInOut),
-          child: child,
-        );
-      },
+      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+          _getFadeTransition(animation, child),
+    );
+  }
+
+  Widget _getFadeTransition(Animation<double> animation, Widget child) {
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+
+  Widget _getSlideTransition(Animation<double> animation, Widget child) {
+    var begin = Offset(0.0, 1.0);
+    var end = Offset.zero;
+    var curve = Curves.ease;
+
+    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+    return SlideTransition(
+      position: animation.drive(tween),
+      child: child,
     );
   }
 }
