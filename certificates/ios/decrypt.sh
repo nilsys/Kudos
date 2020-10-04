6580f3de96238bbed93e9aa7c4d1e790b81f96be
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-10-05 00:21:39 +0300

    Fix decrypt

--- certificates/ios/decrypt.sh
+++ certificates/ios/decrypt.sh
@@ -2,11 +2,11 @@
 echo "Decrypt dev certificate..."
 gpg --quiet --batch --yes --decrypt \
     --passphrase="$IOS_PASSPHRASE" \
-    --output dev.p12 \
+    --output prod.p12 \
     prod.p12.gpg
 
 echo "Decrypt provision profile..."
 gpg --quiet --batch --yes --decrypt \
     --passphrase="$IOS_PASSPHRASE" \
-    --output dev.mobileprovision \
+    --output prod.mobileprovision \
     prod.mobileprovision.gpg
