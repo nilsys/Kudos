65d47e4004982f01d5e5bfaa83a74fb7751be689
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-10-04 23:59:17 +0300

    Rename iOS prod certs

--- certificates/ios/decrypt.sh
+++ certificates/ios/decrypt.sh
@@ -3,10 +3,10 @@ echo "Decrypt dev certificate..."
 gpg --quiet --batch --yes --decrypt \
     --passphrase="$IOS_PASSPHRASE" \
     --output dev.p12 \
-    dev.p12.gpg
+    prod.p12.gpg
 
 echo "Decrypt provision profile..."
 gpg --quiet --batch --yes --decrypt \
     --passphrase="$IOS_PASSPHRASE" \
     --output dev.mobileprovision \
-    dev.mobileprovision.gpg
+    prod.mobileprovision.gpg
--- certificates/ios/encrypt.sh
+++ certificates/ios/encrypt.sh
@@ -1,6 +1,6 @@
 
 echo "Encrypt dev certificate..."
-gpg --symmetric --cipher-algo AES256 dev.p12
+gpg --symmetric --cipher-algo AES256 prod.p12
 
 echo "Encrypt provision profile..."
-gpg --symmetric --cipher-algo AES256 dev.mobileprovision
+gpg --symmetric --cipher-algo AES256 prod.mobileprovision
